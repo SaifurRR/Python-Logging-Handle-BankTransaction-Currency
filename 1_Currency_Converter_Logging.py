755d88134753062d78d6f59d6973bd24563adbdb
SaifurR <90628681+SaifurRR@users.noreply.github.com>
2023-12-09 05:43:53 -0800

    Update 1_Currency_Converter_Logging.py

--- 1_Currency_Converter_Logging.py
+++ 1_Currency_Converter_Logging.py
@@ -1,7 +1,7 @@
 import logging
 import sys    
            
-logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)  
 stream_handler = logging.StreamHandler(sys.stdout)
 formatter = logging.Formatter('[%(asctime)s] %(levelname)s [%(message)s]')
 stream_handler.setFormatter(formatter)
